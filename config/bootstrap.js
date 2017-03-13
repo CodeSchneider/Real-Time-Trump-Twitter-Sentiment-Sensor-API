5bf16a9cacd7aa6904e245d498ebd81d0191c08f
Trey Schneider <treyschneider@Treys-MacBook-Pro.local>
2017-03-13 12:08:14 -0400

    troubleshoot R10 1

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,41 +10,41 @@ module.exports.bootstrap = function(cb) {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || sails.config.twitter.access_token_secret
   });
 
-  var stream = client.stream('statuses/filter', {track: 'donald trump'});
-
-  stream.on('data', function(event) {
-    var r = sentiment(event.text);
-    var picture_url = event.user.profile_image_url;
-    var text = event.text;
-    TweetService.saveTweet(r, picture_url, text, function(err,tweet){
-      if (err) {
-        sails.log.error(err);
-      }
-      return;
-    });
-  });
-
-  stream.on('error', function(error) {
-    sails.log.error(error);
-  });
-
-  setInterval(function() {
-    TweetService.getTickData(function(err,tick){
-      if (err) {
-        sails.log.error(err);
-      }
-      sails.sockets.blast('tick', tick[0]);
-    })
-  }, 2000);
-
-  setInterval(function() {
-    TweetService.getRecentTweets(function(err,tweets){
-      if (err) {
-        sails.log.error(err);
-      }
-      sails.sockets.blast('newRecentTweets', tweets);
-    });
-  }, 5000);
+  // var stream = client.stream('statuses/filter', {track: 'donald trump'});
+  //
+  // stream.on('data', function(event) {
+  //   var r = sentiment(event.text);
+  //   var picture_url = event.user.profile_image_url;
+  //   var text = event.text;
+  //   TweetService.saveTweet(r, picture_url, text, function(err,tweet){
+  //     if (err) {
+  //       sails.log.error(err);
+  //     }
+  //     return;
+  //   });
+  // });
+  //
+  // stream.on('error', function(error) {
+  //   sails.log.error(error);
+  // });
+  //
+  // setInterval(function() {
+  //   TweetService.getTickData(function(err,tick){
+  //     if (err) {
+  //       sails.log.error(err);
+  //     }
+  //     sails.sockets.blast('tick', tick[0]);
+  //   })
+  // }, 2000);
+  //
+  // setInterval(function() {
+  //   TweetService.getRecentTweets(function(err,tweets){
+  //     if (err) {
+  //       sails.log.error(err);
+  //     }
+  //     sails.sockets.blast('newRecentTweets', tweets);
+  //   });
+  // }, 5000);
 
   cb();
 };
